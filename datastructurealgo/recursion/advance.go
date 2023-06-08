1eabd032eb2604ce89f665053b64c2ab52cc607d
Udit Gurani <gurani.udit3@gmail.com>
2023-06-08 15:57:34 +0530

    dsalgo

--- datastructurealgo/recursion/advance.go
+++ datastructurealgo/recursion/advance.go
@@ -10,6 +10,7 @@ func printReverse(str string, n int) {
 	fmt.Println(string(str[0]))
 }
 
+// -------------------------------------------------------
 func findLength(n []int) int {
 	if n == nil && n[1:] != nil {
 		return 1
@@ -19,6 +20,7 @@ func findLength(n []int) int {
 	return smallAnswer
 }
 
+// --------------------------------------------------------
 func replaceCharacter(n string, replaceChar string) string {
 	if len(n) == 0 {
 		return n
@@ -31,6 +33,7 @@ func replaceCharacter(n string, replaceChar string) string {
 	}
 }
 
+// ------------------------------------------------------------
 func removeCharacter(s string, removeChar string) string {
 	if len(s) == 0 {
 		return s
@@ -43,6 +46,7 @@ func removeCharacter(s string, removeChar string) string {
 	}
 }
 
+// --------------------------------------------------------
 func removeConsecutiveDuplicates(s string) string {
 	if len(s) < 2 {
 		return s
@@ -52,3 +56,44 @@ func removeConsecutiveDuplicates(s string) string {
 	}
 	return string(s[0]) + removeConsecutiveDuplicates(s[1:])
 }
+
+// --------------------------------------------------------
+func printSubsequences(str string) {
+	output := ""
+	printSubsequencesHelper(str, output, 0)
+}
+
+func printSubsequencesHelper(str string, output string, index int) {
+	if index == len(str) {
+		if len(output) > 0 {
+			fmt.Println(output)
+		}
+		return
+	}
+
+	// Exclude the current character
+	fmt.Println("Exclude:", str, output, index)
+	printSubsequencesHelper(str, output, index+1)
+
+	// Include the current character
+	fmt.Println("Include:", str, output, index)
+	printSubsequencesHelper(str, output+string(str[index]), index+1)
+}
+
+// ----------------------------------------------------------------------
+func stringToInt(str string) int {
+	return stringToIntRecursive(str, len(str))
+}
+
+func stringToIntRecursive(str string, index int) int {
+	if index == 0 {
+		return 0
+	}
+	smallAns := stringToIntRecursive(str, index-1)
+	lastDigit := str[index-1] - '0'
+	ans := smallAns*10 + int(lastDigit)
+	fmt.Println(smallAns, index, str[index-1], '0', lastDigit, ans)
+	return ans
+}
+
+//-----------------------------------------------------------------------
