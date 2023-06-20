8a3d74704c693be90a7d63d751d21ab4686f7576
Udit Gurani <gurani.udit3@gmail.com>
2023-06-20 19:05:47 +0530

    stack

--- datastructurealgo/stack/linkedlist/stack.go
+++ datastructurealgo/stack/linkedlist/stack.go
@@ -11,8 +11,9 @@ type Node struct {
 }
 
 type Stack struct {
-	top  *Node
-	size int
+	start *Node
+	top   *Node
+	size  int
 }
 
 func NewStack() *Stack {
@@ -20,58 +21,101 @@ func NewStack() *Stack {
 }
 
 func (s *Stack) Push(value int) {
-	newNode := &Node{value: value, next: s.top}
-	s.top = newNode
+	newNode := &Node{value: value}
+
+	if s.start == nil {
+		s.start = newNode
+		s.top = newNode
+	} else {
+		s.top.next = newNode
+		s.top = newNode
+	}
+
 	s.size++
 }
 
 func (s *Stack) Pop() (int, error) {
-	if s.IsEmpty() {
-		return 0, errors.New("stack is empty")
+	if s.top == nil {
+		return 0, errors.New("Stack is empty")
 	}
 
 	value := s.top.value
-	s.top = s.top.next
-	s.size--
 
+	if s.start == s.top {
+		s.start = nil
+		s.top = nil
+	} else {
+		curr := s.start
+		for curr.next != s.top {
+			curr = curr.next
+		}
+		curr.next = nil
+		s.top = curr
+	}
+
+	s.size--
 	return value, nil
 }
 
+func (s *Stack) Print() {
+	curr := s.start
+	for curr != nil {
+		fmt.Println(curr.value)
+		curr = curr.next
+	}
+}
+
 func (s *Stack) Peek() (int, error) {
-	if s.IsEmpty() {
-		return 0, errors.New("stack is empty")
+	if s.top == nil {
+		return 0, errors.New("Stack is empty")
 	}
 
 	return s.top.value, nil
 }
 
-func (s *Stack) IsEmpty() bool {
-	return s.size == 0
-}
+func (s *Stack) MidElement() (int, error) {
+	if s.size == 0 {
+		return 0, errors.New("Stack is empty")
+	}
 
-func (s *Stack) Size() int {
-	return s.size
+	mid := s.size / 2
+	curr := s.start
+
+	for i := 0; i < mid; i++ {
+		curr = curr.next
+	}
+
+	return curr.value, nil
 }
 
 func main() {
 	stack := NewStack()
 
-	// Push elements onto the stack
-	stack.Push(5)
 	stack.Push(10)
-	stack.Push(3)
+	stack.Push(20)
+	stack.Push(30)
+	stack.Push(40)
 
-	// Pop elements from the stack
-	value, _ := stack.Pop()
-	fmt.Println("Popped Value:", value) // Output: 3
+	stack.Print() // Output: 10 20 30 40
 
-	// Peek at the top element of the stack
-	value, _ = stack.Peek()
-	fmt.Println("Top Value:", value) // Output: 10
+	value, err := stack.Pop()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Popped value:", value) // Output: Popped value: 40
+	}
 
-	// Check if the stack is empty
-	fmt.Println("Is Empty:", stack.IsEmpty()) // Output: false
+	value, err = stack.Peek()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Top value:", value) // Output: Top value: 30
+	}
 
-	// Get the size of the stack
-	fmt.Println("Size:", stack.Size()) // Output: 2
+	midValue, err := stack.MidElement()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Middle value:", midValue) // Output: Middle value: 20
+	}
 }
